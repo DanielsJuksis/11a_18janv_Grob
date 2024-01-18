d055bf666199e30f5673760af427197b9260f311
DanielsJuksis <daniels.juksis@grobinasskola.lv>
2024-01-18 15:14:23 +0200

    Init commit

--- 4.uzdevums.py
+++ 4.uzdevums.py
@@ -1,11 +1,8 @@
 def failiņš():
     try:
         fails = input("Ievadiet faila nosaukumu (piemēram, fails.txt): ")
-        
-        # Lietotājs ievada faila formātu (paplašinājumu)
         formāts = input("Ievadiet faila formātu (piemēram, txt): ")
         
-        # Apvienojam nosaukumu un formātu, lai iegūtu pilnu ceļu
         atrašanās = f"{fails}.{formāts}"
 
         with open(atrašanās, 'r', encoding='utf-8') as fails:
